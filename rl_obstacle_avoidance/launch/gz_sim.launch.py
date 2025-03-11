22f26329405807dabd2947bfaf61c96ee147c27d
Erik Smit <erikgsmit@gmail.com>
2025-03-11 17:22:50 +0100

    feat: added a bridge between gz and ros2 topics, closes #27

--- rl_obstacle_avoidance/launch/gz_sim.launch.py
+++ rl_obstacle_avoidance/launch/gz_sim.launch.py
@@ -4,6 +4,7 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ros_gz_bridge.actions import RosGzBridge
 
 
 def generate_launch_description():
@@ -11,14 +12,17 @@ def generate_launch_description():
     pkg_rl_drone = get_package_share_directory('rl_obstacle_avoidance')
 
     world_file = PathJoinSubstitution([pkg_rl_drone, 'worlds', 'drone_world.sdf'])
-    # model_file = PathJoinSubstitution([pkg_rl_drone, 'models', 'parrot_bebop_2', 'model.sdf'])
+    bridge_config = PathJoinSubstitution([pkg_rl_drone, 'config', 'bridge.yaml'])
+
+    # Declare bridge config as a launch argument
+    declare_bridge_config = DeclareLaunchArgument(
+        'config_file',
+        default_value=bridge_config,
+        description='YAML file for ROS-Gazebo bridge'
+    )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'world',
-            default_value=world_file,
-            description='Path to the Gazebo world file'
-        ),
+        declare_bridge_config,  # Add bridge argument
 
         # Launch Gazebo with the world file
         IncludeLaunchDescription(
@@ -28,5 +32,9 @@ def generate_launch_description():
             launch_arguments={'gz_args': world_file}.items(),
         ),
 
-       
+        # Start ROS-Gazebo Bridge
+        RosGzBridge(
+            bridge_name="ros_gz_bridge",
+            config_file=bridge_config,
+        ),
     ])
