44f891f0b267085e9f1372a9c8db0f487cac2f5e
Erik Smit <erikgsmit@gmail.com>
2025-03-25 14:11:41 +0100

    feat: added external parameters file, also did some training, closes #58

--- rl_obstacle_avoidance/setup.py
+++ rl_obstacle_avoidance/setup.py
@@ -14,7 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml') + glob("config/*.json")),
+
         
         # X3 UAV
         (os.path.join('share', package_name, 'models/x3'), 
